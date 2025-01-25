a5582863bcea593a8bb58a30807903a6e1d03e5c
eliott <backup2@eliottdesk>
2025-01-25 12:00:30 -0700

    simple s3 state, just one job

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ provider "nomad" {
   address = "https://bigo-server-oracle.tail800b49.ts.net/"
 }
 
-resource "nomad_job" "run-go-binary" {
-  jobspec = file("./jobs/run-go-binary.hcl")
-}
-
 resource "nomad_job" "monad-forwarder" {
   jobspec = file("./jobs/monad-forwarder.hcl")
 }
