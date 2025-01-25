11cb7821fbcdb2889f794407386339b2f593a52a
eliott <backup2@eliottdesk>
2025-01-24 21:12:45 -0700

    variable defaults

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "nomad_token" {
   description = "Nomad ACL token"
   type        = string
+  default     = "wrong_token"
 }
 
 variable "job_name" {
   description = "Name of the job"
   type        = string
+  default     = "wrong_name"
 }
 
 variable "datacenter" {
