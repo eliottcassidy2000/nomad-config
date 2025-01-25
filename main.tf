32e8ba0d8214a6e14e22570c68a5f9df31c416c2
eliott <backup2@eliottdesk>
2025-01-25 11:56:54 -0700

    removing weed stuff for now

--- main.tf
+++ main.tf
@@ -2,18 +2,6 @@ provider "nomad" {
   address = "https://bigo-server-oracle.tail800b49.ts.net/"
 }
 
-resource "nomad_job" "weed-volume" {
-  jobspec = file("./jobs/weed-volume.hcl")
-}
-
-resource "nomad_job" "weed-server" {
-  jobspec = file("./jobs/weed-server.hcl")
-}
-
-resource "nomad_job" "weed-master" {
-  jobspec = file("./jobs/weed-master.hcl")
-}
-
 resource "nomad_job" "run-go-binary" {
   jobspec = file("./jobs/run-go-binary.hcl")
 }
