3644eb4107eec0ba20a5b5e57dd8f429d380e61a
eliott <backup2@eliottdesk>
2025-01-31 15:39:57 -0700

    seeing if this removed jobs

--- main.tf
+++ main.tf
@@ -16,14 +16,14 @@ resource "nomad_job" "hello-world" {
   jobspec = file("./jobs/hello-world.hcl")
 }
 
-resource "nomad_job" "weed-master" {
-  jobspec = file("./jobs/weed-master.hcl")
-}
+# resource "nomad_job" "weed-master" {
+#   jobspec = file("./jobs/weed-master.hcl")
+# }
 
-resource "nomad_job" "weed-server" {
-  jobspec = file("./jobs/weed-server.hcl")
-}
+# resource "nomad_job" "weed-server" {
+#   jobspec = file("./jobs/weed-server.hcl")
+# }
 
-resource "nomad_job" "weed-mvolume" {
-  jobspec = file("./jobs/weed-volume.hcl")
-}
+# resource "nomad_job" "weed-mvolume" {
+#   jobspec = file("./jobs/weed-volume.hcl")
+# }
