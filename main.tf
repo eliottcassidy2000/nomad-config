6f1b15b35ef44547f28d66898a1e22690a9a4c4f
Eliott Cassidy <110405487+eliottcassidy2000@users.noreply.github.com>
2025-04-22 13:17:06 -0600

    minimal

--- main.tf
+++ main.tf
@@ -5,22 +5,7 @@ provider "nomad" {
 
 resource nomad_dynamic_host_volume "mysql_data" {
   name      = "mysql_data"
-  namespace = "prod"
   plugin_id = "mkdir"
-
-  capacity_max = "12 GiB"
-  capacity_min = "1.0 GiB"
-
-  capability {
-    access_mode     = "single-node-writer"
-    attachment_mode = "file-system"
-  }
-
-  constraint {
-    attribute = "$${attr.kernel.name}"
-    value     = "linux"
-  }
-
 }
 
 #resource "nomad_job" "monad-forwarder" {
